ca7a24c20aeb89de9840cffadd834fa2d17f4177
imhphari <imhphari@gmail.com>
2018-11-27 13:45:54 +0530

    Defining the routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { RouterModule } from '@angular/router';
 import { AngularFireDatabaseModule } from 'angularfire2/database';
 import { environment } from './../environments/environment';
 import { BrowserModule } from '@angular/platform-browser';
@@ -15,6 +16,8 @@ import { CheckOutComponent } from './check-out/check-out.component';
 import { OrderSuccessComponent } from './order-success/order-success.component';
 import { MyOrdersComponent } from './my-orders/my-orders.component';
 import { AdminProductsComponent } from './admin/admin-products/admin-products.component';
+import { AdminOrdersComponent } from './admin/admin-orders/admin-orders.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
@@ -26,14 +29,28 @@ import { AdminProductsComponent } from './admin/admin-products/admin-products.co
     CheckOutComponent,
     OrderSuccessComponent,
     MyOrdersComponent,
-    AdminProductsComponent
+    AdminProductsComponent,
+    AdminOrdersComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     AngularFireModule.initializeApp(environment.firebase),
     AngularFireAuthModule,
-    AngularFireDatabaseModule
+    AngularFireDatabaseModule,
+    RouterModule.forRoot([
+      {path:'' ,component:HomeComponent},
+      {path:'products' ,component:ProductsComponent},
+      {path:'shopping-cart' ,component: ShoppingCartComponent},
+      {path:'check-out' ,component:CheckOutComponent},
+      {path:'order-success' ,component:OrderSuccessComponent},
+      {path:'login' ,component:LoginComponent},
+      {path:'admin/products' ,component:AdminProductsComponent},
+      {path:'admin/orders' ,component:AdminOrdersComponent}
+      
+     
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
